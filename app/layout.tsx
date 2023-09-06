bc74ebb24238f4597212c354d63ee742fce31d34
KaliMerO666 <kalimero@ik.me>
2023-09-06 10:58:54 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "../global.css";
-import { Inter } from "@next/font/google";
+import { Alata } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
 		shortcut: "/favicon.png",
 	},
 };
-const inter = Inter({
+const Alata = Alata ({
 	subsets: ["latin"],
 	variable: "--font-inter",
 });
