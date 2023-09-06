06b9cbb036a10e4e92f4b49054296af7b3bd14aa
KaliMerO666 <kalimero@ik.me>
2023-09-06 11:44:28 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" className={[inter.variable, AccidentalPresidency.variable].join(" ")}>
+		<html lang="en" className={[inter.variable, Alata.variable].join(" ")}>
 			<head>
 				<Analytics />
 			</head>
